c3eb5f8e9c5937ee2f9e430d9138eb35f3254b82
dsouzaankit <dsouzaankit@gmail.com>
2026-04-23 23:42:46 -0400

    added json aggregated output report

--- script/sql/transformations.sql
+++ script/sql/transformations.sql
@@ -21,42 +21,60 @@ with book_info as (
     embedding as book_embedding
     from staging_books
 )
-select
-    -- jds.requirements_headers as jd_requirements_headers,
-    jds.chunk_id as jd_chunk_id,
-    left(jds.document, 100) as jd_chunk_text_1st100chars,
-    right(jds.document, 100) as jd_chunk_text_last100chars,
-    -- book_info.book_chunk_1st100chars as book_chunk_1st100chars,
-    book_info.book_section_title_chunk_debug as book_section_title_chunk_debug,
-    book_info.book_title as book_title,
-    book_info.book_chapter_number as book_chapter_number,
-    book_info.book_chapter_title as book_chapter_title,
-    -- book_info.book_section_index as book_section_index,
-    -- jds.requirements_headers as jd_requirements_headers,
-    -- books.document as book_chunk_text,
-    array_cosine_similarity(
-        jds.embedding::DOUBLE[384],
-        book_info.book_embedding::DOUBLE[384]
-    ) as cosine_similarity
-    , book_info.book_chunk_id as book_chunk_id
-from staging_jd jds, book_info
-where jds.requirements_headers != jds.document  -- ignore matches to requirement related section headers in jd
-and jds.requirements_headers not like '%' || jds.document || '%'    -- ignore matches to requiremnent related section headers in jd
-and jds.requirements_headers not like '%' || book_info.book_section_title_chunk_debug || '%'    -- ignore matches to requiremnent related section headers in jd
--- and lower(jds.document) not in ('requirements', 'work authorization', 'equal opportunity', 'eeo')
-and not regexp_matches(jds.document, 'requirements|work authorization|authorized|equal opportunity|eeo|disability|401k|dental', 'i')
-and not regexp_matches(jds.document, 'qualifications|values|benefits|compensation|salary|pay range|opportunity|position|role', 'i')
-and array_cosine_similarity(
-        jds.embedding::DOUBLE[384],
-        book_info.book_embedding::DOUBLE[384]
-    ) > 0.5
--- and jds.chunk_id like '4335399466%'
--- and jds.chunk_id like '4363131526%'
--- and jds.chunk_id like '4387419689%'
--- and jds.chunk_id like '4387975674%'
--- and jds.chunk_id like '4387978601%'
-qualify rank() over (partition by jds.chunk_id order by cosine_similarity desc) <= 3
-order by jds.chunk_id, cosine_similarity desc
+, flat_report as (
+    select
+        -- jds.requirements_headers as jd_requirements_headers,
+        jds.chunk_id as jd_chunk_id,
+        replace(left(jds.document, 100), chr(10), ' ') as jd_chunk_text_1st100chars,
+        replace(right(jds.document, 100), chr(10), ' ') as jd_chunk_text_last100chars,
+        -- book_info.book_chunk_1st100chars as book_chunk_1st100chars,
+        book_info.book_section_title_chunk_debug as book_section_title_chunk_debug,
+        book_info.book_title as book_title,
+        book_info.book_chapter_number as book_chapter_number,
+        book_info.book_chapter_title as book_chapter_title,
+        -- book_info.book_section_index as book_section_index,
+        -- jds.requirements_headers as jd_requirements_headers,
+        -- books.document as book_chunk_text,
+        array_cosine_similarity(
+            jds.embedding::DOUBLE[384],
+            book_info.book_embedding::DOUBLE[384]
+        ) as cosine_similarity
+        , book_info.book_chunk_id as book_chunk_id
+    from staging_jd jds, book_info
+    where jds.requirements_headers != jds.document  -- ignore matches to requirement related section headers in jd
+    and jds.requirements_headers not like '%' || jds.document || '%'    -- ignore matches to requiremnent related section headers in jd
+    and jds.requirements_headers not like '%' || book_info.book_section_title_chunk_debug || '%'    -- ignore matches to requiremnent related section headers in jd
+    -- and lower(jds.document) not in ('requirements', 'work authorization', 'equal opportunity', 'eeo')
+    and not regexp_matches(jds.document, 'requirements|work authorization|authorized|equal opportunity|eeo|disability|401k|dental', 'i')
+    and not regexp_matches(jds.document, 'qualifications|values|benefits|compensation|salary|pay range|opportunity|position|role', 'i')
+    and array_cosine_similarity(
+            jds.embedding::DOUBLE[384],
+            book_info.book_embedding::DOUBLE[384]
+        ) > 0.5
+    -- and jds.chunk_id like '4335399466%'
+    -- and jds.chunk_id like '4363131526%'
+    -- and jds.chunk_id like '4387419689%'
+    -- and jds.chunk_id like '4387975674%'
+    -- and jds.chunk_id like '4387978601%'
+    qualify rank() over (partition by jds.chunk_id order by cosine_similarity desc) <= 3
+    order by jds.chunk_id, cosine_similarity desc
+)
+
+select jd_chunk_id, jd_chunk_text_1st100chars, jd_chunk_text_last100chars
+  , json_pretty(json_group_array(
+    json_object(
+      'book_section_title_chunk_debug', book_section_title_chunk_debug,
+      'book_title', book_title,
+      'book_chapter_number', book_chapter_number,
+      'book_chapter_title', book_chapter_title,
+      'cosine_similarity', cosine_similarity,
+      'book_chunk_id', book_chunk_id
+    )
+  )) as book_info
+from flat_report
+group by 1,2,3
+order by 1,2,3
+
 limit 10
 ;
 
